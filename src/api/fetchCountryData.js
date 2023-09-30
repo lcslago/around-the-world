50f1454b3e5e19ec518235b50684b52ace85d194
lcslago <lcslago97@gmail.com>
2023-09-30 15:35:54 -0300

    refactor: mudanças no código para lidar com enventuais erros de request

--- src/api/fetchCountryData.js
+++ src/api/fetchCountryData.js
@@ -5,23 +5,28 @@ const versionOfAPI = "v3.1"
 const endpointOfAPI = `https://restcountries.com/${versionOfAPI}/`;
 
 export async function fetchCountryData() {
-    let responseArr = [];
-    const response = (await fetch(`${endpointOfAPI}all`));
-    responseArr = await response.json();
-    countryDataFilter.forEach(filter => filter.hidden = false);
+    try {
+        let responseArr = [];
+        const response = (await fetch(`${endpointOfAPI}all`));
+        responseArr = await response.json();
+        countryDataFilter.forEach(filter => filter.hidden = false);
 
-    const cardsPerScroll = 16;
-    let currentPosition = 0;
-    let currentBatch = cardsPerScroll;
-    showCountryData(responseArr, currentPosition, currentBatch);
+        const cardsPerScroll = 16;
+        let currentPosition = 0;
+        let currentBatch = cardsPerScroll;
+        showCountryData(responseArr, currentPosition, currentBatch);
 
-    /* infinity scrolling */
-    window.addEventListener('scroll', () => {
-        if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 500) {
-            currentPosition = currentPosition += cardsPerScroll;
-            currentBatch = currentBatch += cardsPerScroll;
-            showCountryData(responseArr, currentPosition, currentBatch);
-        }
-    });
+        /* infinity scrolling */
+        window.addEventListener('scroll', () => {
+            if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 500) {
+                currentPosition = currentPosition += cardsPerScroll;
+                currentBatch = currentBatch += cardsPerScroll;
+                showCountryData(responseArr, currentPosition, currentBatch);
+            }
+        });
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
 
 }
