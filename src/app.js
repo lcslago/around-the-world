031524d246730772aa80dc4ac505c1868e08f5d5
lcslago <lcslago97@gmail.com>
2023-10-18 19:25:45 -0300

    feat: países são randomizados a cada refresh na página inicial

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import { renderHomePage } from "./utils/renderHomePage.js";
+import { renderHomePage, renderScrollUpButton } from "./utils/renderHomePage.js";
 import { setTheme } from "./utils/setTheme.js";
 
 (() => {
     renderHomePage();
     setTheme();
+    renderScrollUpButton();
 })()
--- src/utils/renderHomePage.js
+++ src/utils/renderHomePage.js
@@ -2,23 +2,21 @@ import { fetchCountryData } from "../api/fetchCountryData.js";
 import { renderCountryData } from "./../api/renderCountryData.js";
 import { filterCountryData, checkTheFilter } from "./../api/filterCountryData.js";
 
-const $ = document.querySelector.bind(document);
-
 export async function renderHomePage() {
     const responseArr = await fetchCountryData();
+    const sortedData = shuffleCountryData(responseArr);
 
     const cardsPerScroll = 16;
     let currentPosition = 0;
     let currentBatch = cardsPerScroll;
-    showCountryData(responseArr, currentPosition, currentBatch);
-    filterCountryData(responseArr);
+    showCountryData(sortedData, currentPosition, currentBatch);
+    filterCountryData(sortedData);
 
     infinityScrolling(
-        responseArr,
+        sortedData,
         currentPosition,
         currentBatch,
         cardsPerScroll);
-
 }
 
 function infinityScrolling(arr, currentPosition, currentBatch, cardsPerScroll) {
@@ -30,7 +28,6 @@ function infinityScrolling(arr, currentPosition, currentBatch, cardsPerScroll) {
                 showCountryData(arr, currentPosition, currentBatch);
             }
         }
-        renderScrollUpButton();
     });
 }
 
@@ -42,17 +39,28 @@ function showCountryData(arr, currentPosition, currentBatch) {
     }
 }
 
-function renderScrollUpButton() {
-    const scrollUpButton = $('[data-scroll-up]');
+export function renderScrollUpButton() {
+    window.addEventListener('scroll', () => {
+        const scrollUpButton = document.querySelector('[data-scroll-up]');
 
-    window.scrollY === 0 ?
-        scrollUpButton.hidden = true :
-        scrollUpButton.hidden = false;
+        window.scrollY === 0 ?
+            scrollUpButton.hidden = true :
+            scrollUpButton.hidden = false;
 
-    scrollUpButton.addEventListener('click', () => {
-        window.scrollBy({
-            top: -window.innerHeight - 1000000,
-            behavior: "instant"
+        scrollUpButton.addEventListener('click', () => {
+            window.scrollBy({
+                top: -window.innerHeight - 1000000,
+                behavior: "instant"
+            })
         })
     });
+}
+
+//algoritmo de ordernação aleatória fisher-yates 
+function shuffleCountryData(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
 }
