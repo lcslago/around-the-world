9dc69443859093d6f4779acc74f8b12fb06540b4
lcslago <lcslago97@gmail.com>
2023-10-27 19:35:52 -0300

    perf: adicionado web workers para melhoria no fetch de dados

--- src/api/fetchCountryData.js
+++ src/api/fetchCountryData.js
@@ -1,15 +1,29 @@
 const versionOfAPI = "v3.1"
 const endpointOfAPI = `https://restcountries.com/${versionOfAPI}/`;
 
-export async function fetchCountryData() {
+async function fetchCountryData() {
     try {
         let responseArr = [];
         const response = (await fetch(`${endpointOfAPI}all`));
         responseArr = await response.json();
 
-        return responseArr;
+        const shuffledData = shuffleCountryData(responseArr);
+        postMessage(shuffledData);
 
     } catch (error) {
         console.error('Error:', error);
     }
 }
+
+//algoritmo de ordernação aleatória fisher-yates 
+function shuffleCountryData(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+addEventListener('message', () => {
+    fetchCountryData();
+})
