aad53a10253584c5c67cfe214ae257e4f1cf87dd
lcslago <lcslago97@gmail.com>
2023-11-28 19:18:06 -0300

    fix: shuffle volta a acontecer a cada visita

--- src/api/fetchCountryData.js
+++ src/api/fetchCountryData.js
@@ -17,10 +17,11 @@ async function fetchCountryData() {
 
 //algoritmo de ordernação aleatória fisher-yates 
 function shuffleCountryData(arr) {
-    // for (let i = arr.length - 1; i > 0; i--) {
-    //     const j = Math.floor(Math.random() * (i + 1));
-    //     [arr[i], arr[j]] = [arr[j], arr[i]];
-    // }
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+
     return arr;
 }
 
