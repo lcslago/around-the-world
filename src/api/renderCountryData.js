d8640e4407025d866812553719828b207cea67f3
lcslago <lcslago97@gmail.com>
2023-12-04 19:12:21 -0300

    style: página 404 reformulada

--- src/api/renderCountryData.js
+++ src/api/renderCountryData.js
@@ -4,6 +4,19 @@ export function renderCountryData(data, type) {
     const bodyPage = document.querySelector('[data-country-cards]');
     const searchBar = document.querySelector('[data-search]');
 
+    const resultErrorIcon = () => `
+                            <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-x-circle p-2" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                            </svg>`
+    const resultErrorMessage = (message) => `
+                            <div class="container d-flex flex-column text-center gap-1">
+                                <h2 class="nunito-bolder">Result not found</h2>
+                                <p class="col-lg-5 m-auto">
+                                    ${message}
+                                </p>
+                            </div>`
+
     switch (type) {
         case 'card':
             const countryName = data.name.common;
@@ -53,16 +66,9 @@ export function renderCountryData(data, type) {
             const divElement = bodyPage.appendChild(document.createElement("div"));
             divElement.classList.add("w-100");
             divElement.innerHTML = `
-                    <div class="d-flex justify-content-left gap-lg-5 search-404">
-                            <img src='./src/assets/img/search-off.svg' alt="Result not found" class="search-off-icon"/>
-                            <div class="container d-flex flex-column justify-content-center">
-                                <h2 class="nunito-bolder">Result not found</h2>
-                                <hr class="col-lg-6">
-                                <p class="col-lg-6">
-                                    Your search - <strong>${searchBar.value}</strong> - did not match any country.
-                                    Please, make sure all words are spelled correctly.
-                                </p>
-                            </div>
+                    <div class="d-flex flex-column align-items-center gap-3 search-404">
+                            ${resultErrorIcon()}
+                            ${resultErrorMessage(`Your search - <strong>${searchBar.value}</strong> - did not match any country. Please, make sure all words are spelled correctly.`)}
                     </div>
                     `
             break;
@@ -71,16 +77,10 @@ export function renderCountryData(data, type) {
             const filterDivElement = bodyPage.appendChild(document.createElement("div"));
             filterDivElement.classList.add("w-100");
             filterDivElement.innerHTML = `
-                    < div class="d-flex justify-content-left gap-lg-5 search-404" >
-                            <img src='./src/assets/img/search-off.svg' alt="Result not found" class="search-off-icon"/>
-                            <div class="container d-flex flex-column justify-content-center">
-                                <h2 class="nunito-bolder">Country not found</h2>
-                                <hr class="col-lg-6">
-                                <p class="col-lg-6">
-                                    There was no country from <strong>${returnRegionValue()}</strong> in your search results.
-                                </p>
-                            </div>
-                        </div >
+                    <div class="d-flex flex-column align-items-center gap-3 search-404">
+                            ${resultErrorIcon()}
+                            ${resultErrorMessage(`There was no country from <strong>${returnRegionValue()}</strong> in your search results.`)}
+                        </div>
                     `
             break;
     }
