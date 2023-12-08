2bd38fd8c422e89ce4757f9f42056a1bd5c095d1
lcslago <lcslago97@gmail.com>
2023-12-08 18:48:12 -0300

    refactor: svgs centralizados em um unico arquivo js

--- src/api/renderCountryData.js
+++ src/api/renderCountryData.js
@@ -68,11 +68,6 @@ export function renderCountryData(data, type) {
 }
 
 function renderErrorMessage(message) {
-    const resultErrorIcon = () => `
-                            <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-x-circle p-2" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                            </svg>`
     const resultErrorMessage = (message) => `
                             <div class="container d-flex flex-column text-center gap-1">
                                 <h2 class="nunito-bolder">Result not found</h2>
@@ -87,11 +82,9 @@ function renderErrorMessage(message) {
     return (
         divElement.innerHTML = `
                     <div class="d-flex flex-column align-items-center gap-3 search-404">
-                            ${resultErrorIcon()}
+                            ${resultErrorIcon}
                             ${resultErrorMessage(`${message}`)}
                     </div>
                     `
     )
 }
-
-
